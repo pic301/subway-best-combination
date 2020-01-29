1c6ff57cddd663fd90cebca5c727b278f4f6f2a9
pic301 <ppic301@gmail.com>
2020-01-29 20:41:42 +0900

    나만의 꿀조합  combination page
    이미지 슬라이더 완성

--- src/pages/Combination.js
+++ src/pages/Combination.js
@@ -4,11 +4,12 @@ import order00 from "../images/combination/order00.png";
 import order01 from "../images/combination/order01.png";
 import order02 from "../images/combination/order02.png";
 import order03 from "../images/combination/order03.png";
+import order04 from "../images/combination/order04.png";
 
 
 class Combination extends Component {
   state = {
-    images: [order00, order01, order02, order03,],
+    images: [order00, order01, order02, order03, order04],
     step: 0,
     materials:
     [
@@ -16,18 +17,26 @@ class Combination extends Component {
       ["베이컨 비츠","쉬림프","에그마요","오믈렛","아보카도","페퍼로니","베이컨"],
       ["양상추","토마토","오이","피망","양파","피클","올리브","할라피뇨","아보카도"],
       ["아메리칸 치즈", "슈레드치즈", "모차렐라 치즈"],
-      // ["랜치드레싱","마요네즈","스위트 어니언","허니 머스타드", "스위트 칠리", "핫 칠리", "사우스 웨스트", "머스타드", "홀스래디쉬",
-      // "사우전 아일랜드 ","이탈리안 드레싱" ,"올리브오일", "레드와인식초" ,"소금", "후추", "스모크 바비큐"]
+      ["랜치드레싱","마요네즈","스위트 어니언","허니 머스타드", "스위트 칠리", "핫 칠리", "사우스 웨스트", "머스타드", "홀스래디쉬",
+       "사우전 아일랜드 ","이탈리안 드레싱" ,"올리브오일", "레드와인식초" ,"소금", "후추", "스모크 바비큐"]
     ],
     combination:[]
   };
   nextStep = () => {
-    this.setState({ step: this.state.step + 1 });
-
+    // this.state.step > 4
+    // ?this.setState({ step: this.state.step === 0})
+    // :this.setState({ step: this.state.step + 1 });
+    if( this.state.step > 3){
+      this.setState({ step: 0})
+    } else{
+      this.setState({ step: this.state.step + 1})
+    }
     console.log(this.state.step);
   };
   prevStep = () => {
-    this.setState({ step: this.state.step - 1 });
+    this.state.step === 0     
+    ?this.setState({ step: 3})
+    :this.setState({ step: this.state.step - 1 });
     console.log(this.state.step);
   };
   onClick = (e) => {
