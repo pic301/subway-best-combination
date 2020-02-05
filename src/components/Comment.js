c8c5e755a884a4125ab403a326c4450dc9e7a710
pic301 <ppic301@gmail.com>
2020-02-06 01:59:17 +0900

    Comment 기능 완성
    댓글등록 ,삭제 구현 , user image 추가 ,css적용

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,55 +1,116 @@
 import React, { Component } from "react";
+import "./Comment.css";
+import user from '../images/user/user.png'
 
 export default class CommentBox extends Component {
-  state = {};
+  state = {
+    open: false,
+    comments: [
+      { id: 1, author: "써브웨이 매니아", text: "서브웨이 엄청좋아요" },
+      { id: 2, author: "샌드위치 좋아", text: "서브웨이 맛있어요" },
+      { id: 3, author: "I love subway", text: "서브웨이는 짱이에요" }
+    ]
+  };
+
+  _addComment = (author, text) => {
+    const comment = {
+      id: this.state.comments.length + 1,
+      author,
+      text
+    };
+    this.setState({ comments: this.state.comments.concat([comment]) });
+  };
+   _deleteComment = (id) => {
+    this.setState({comments:this.state.comments.filter(
+      comment => comment.id !== id)})
+    
+  }
+  _getComments = () => {
+    this.state.comments.map(comment => (
+      <Comment key={comment.id}  author={comment.author} text={comment.text} />
+    ));
+  };
+ 
+  _getCommentsTitle = commentCount => {
+    if (commentCount === 0) {
+      return "댓글이 없습니다";
+    } else if (commentCount === 1) {
+      return "1 comment";
+    } else {
+      return `${commentCount} comments`;
+    }
+  };
+
   render() {
+    const commentCount = this.state.comments.length;
     return (
       <>
         <div className="comment-box">
-          <span>꿀조합</span>
-          <span>게시판</span>
+          <span className="comment-box-left">꿀조합</span>
+          <span className="comment-box-right"> 후기</span>
+          <CommentForm addComment={this._addComment} />
+          <h3>댓글</h3>
+          <h4 className="comment-count">
+            {this._getCommentsTitle(commentCount)}
+          </h4>
+        {   this.state.comments.map(comment => (
+        <Comment key={comment.id} id={comment.id} deleteComment={this._deleteComment}  author={comment.author} text={comment.text} />
+        ))};
         </div>
-        <CommentForm />
-        <button id onClick>
-          댓글 보기        
-        </button>
-        <h3>댓글</h3>
-        <h4 className="comment-count">댓글숫자</h4>
       </>
     );
   }
 }
 
 class CommentForm extends Component {
+  _handleSubmit = e => {
+    e.preventDefault();
+    let author = this._author;
+    let text = this._text;
+    this.props.addComment(author.value, text.value);
+  };
   render() {
     return (
-      <form className="comment-form" onSubmit>
+      <form className="comment-form" onSubmit={this._handleSubmit}>
         <div className="comment-form-fields">
-          <input type="text" />
-          <textarea  rows="4"></textarea>
+          <input
+            placeholder="Name"
+            type="text"
+            required
+            ref={input => {
+              this._author = input;
+            }}
+          />
+          <textarea
+            placeholder="Comment"
+            rows="4"
+            required
+            ref={textarea => {
+              this._text = textarea;
+            }}
+          ></textarea>
         </div>
         <div className="comment-form-actions">
-            <button type="submit">댓글 등록</button>
-          </div>
+          <button type="submit">댓글 등록</button>
+        </div>
       </form>
     );
   }
 }
 
-class Comment extends Component{
-  render(){
+class Comment extends Component {
+
+  render() {
+    const { id, deleteComment } = this.props
     return (
       <div className="comment">
-          <div className="comment-header">
-
-          </div>
-          <div className="comment-body">
-
-          </div>
-          <div className="comment-footer">
-
-          </div>
+        <img src={user} alt="user-image"/>
+        <p className="comment-author">{this.props.author}</p>
+        <p className="comment-text">{this.props.text}</p>
+        <div className="comment-delete">
+            <button  onClick={() => deleteComment(id)}>삭제</button>
+        </div>
       </div>
-    )
+    );
   }
 }
