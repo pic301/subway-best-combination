3886b38c7c7b9be973d521f1c4172bc49c7304f1
pic301 <ppic301@gmail.com>
2020-02-12 01:08:12 +0900

    Post page init

---
+++ src/pages/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <div>
+                포스트
+            </div>
+        );
+    }
+}
+
+export default Post;
