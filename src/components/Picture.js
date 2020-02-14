9cc25fabaafdd6ecb90d6f476a849d1c126f6b58
pic301 <ppic301@gmail.com>
2020-02-14 14:21:53 +0900

    Picture 컴포넌트 init

---
+++ src/components/Picture.js
@@ -0,0 +1,16 @@
+ import React from 'react'
+ import { store } from '../components/firebaseConfig'
+ 
+ const Picture = () => {
+    const [image,setImage] = useState('')
+    const [url,setUrl] = useState('')
+    const [images,setImages] = useState([])
+  
+ 
+    return (
+      <div>
+       
+      </div>
+    );
+  };
+  export default Picture
