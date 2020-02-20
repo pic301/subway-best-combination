4a43a73ee91009570784b7c3f6ddd84a9603b6bb
pic301 <ppic301@gmail.com>
2020-02-21 06:35:47 +0900

    franchiseInquiry page  개인정보 동의 체크박스 , title
    material-ui 를 사용해서 체크박스 구현

--- src/pages/franchiseInquiry.js
+++ src/pages/franchiseInquiry.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import "./franchiseInquiry.css"
 
-const franchiseInquiry = () => {
+import Checkbox from '@material-ui/core/Checkbox'
+
+const FranchiseInquiry = () => {
+    const [checked, setChecked] = useState(true);
+
+    const handleChange = e => {
+       setChecked(e.target.checked);
+    };
     return (
-        <div>
-            franchiseInquiry
+        <div className="franchise">
+            <div className="franchise-bg">
+                <div className="franchise-header">
+                    <div className="franchise-title">
+                        가맹신청ㆍ문의
+                    </div>
+                  <div className="franchise-privacy">
+                    <div>
+                    <p>개인정보수집 및 이용동의</p>
+                    <textarea className="franchise-privacy-left" id="" cols="30" rows="10">
+                    프랑시스 마디씩 많은 하나에 지나고 사랑과 불러 있습니다. 비둘기, 남은 별이 이제 우는 토끼, 오면 언덕 거외다. 하나에 이제 이름과, 때 소학교 시와 버리었습니다. 새겨지는 많은 소녀들의 듯합니다. 별 사람들의 별들을 이름과, 거외다. 이네들은 별에도 부끄러운 겨울이 멀듯이, 있습니다. 헤일 별 묻힌 풀이 어머님, 책상을 별이 밤이 패, 까닭입니다. 언덕 아침이 별 어머니, 까닭입니다. 오는 나는 나는 소녀들의 까닭입니다. 별이 아이들의 벌써 봅니다.
+
+                    북간도에 차 것은 속의 벌레는 무덤 오면 계십니다. 당신은 어머니, 묻힌 무덤 피어나듯이 새겨지는 소학교 이름을 노루, 듯합니다. 그리워 흙으로 가을 사람들의 별에도 부끄러운 이국 하나 있습니다. 아스라히 벌레는 별이 봅니다. 봄이 까닭이요, 흙으로 어머님, 이름을 거외다. 불러 걱정도 딴은 다 이름과, 별이 책상을 거외다. 남은 새워 이름과, 내 멀듯이, 거외다. 까닭이요, 이웃 하나의 봄이 까닭입니다. 벌써 별 내일 별 동경과 보고, 어머님, 아침이 까닭입니다. 이름을 위에 경, 풀이 노루, 라이너 별들을 않은 없이 거외다.
+
+                    강아지, 풀이 못 밤을 있습니다. 아무 하나에 쓸쓸함과 별 슬퍼하는 하늘에는 봅니다. 나의 내 쓸쓸함과 북간도에 어머니, 까닭입니다. 내린 이웃 나는 시인의 사람들의 있습니다. 까닭이요, 한 이름자를 그리고 듯합니다. 쉬이 무성할 아침이 옥 것은 사랑과 별 언덕 별들을 있습니다. 별 노새, 사랑과 파란 마리아 흙으로 내 라이너 가슴속에 까닭입니다. 오면 아무 벌레는 별 거외다. 까닭이요, 릴케 나는 봅니다. 이름자를 덮어 프랑시스 다하지 거외다. 하나에 잠, 언덕 슬퍼하는 이름과, 있습니다.
+                    </textarea>
+                    
+                    <p>
+                    <Checkbox
+                      checked={checked}
+                      onChange={handleChange}
+                      value="primary"
+                      inputProps={{ 'aria-label': 'primary checkbox' }}
+                    />개인정보수집 및 이용에 동의합니다.(필수)</p>
+                    </div>
+                    <div>
+                    <p>개인정보 위탁동의</p>
+                    <textarea className="franchise-privacy-right" id="" cols="30" rows="10" >
+                                        프랑시스 마디씩 많은 하나에 지나고 사랑과 불러 있습니다. 비둘기, 남은 별이 이제 우는 토끼, 오면 언덕 거외다. 하나에 이제 이름과, 때 소학교 시와 버리었습니다. 새겨지는 많은 소녀들의 듯합니다. 별 사람들의 별들을 이름과, 거외다. 이네들은 별에도 부끄러운 겨울이 멀듯이, 있습니다. 헤일 별 묻힌 풀이 어머님, 책상을 별이 밤이 패, 까닭입니다. 언덕 아침이 별 어머니, 까닭입니다. 오는 나는 나는 소녀들의 까닭입니다. 별이 아이들의 벌써 봅니다.
+
+                    북간도에 차 것은 속의 벌레는 무덤 오면 계십니다. 당신은 어머니, 묻힌 무덤 피어나듯이 새겨지는 소학교 이름을 노루, 듯합니다. 그리워 흙으로 가을 사람들의 별에도 부끄러운 이국 하나 있습니다. 아스라히 벌레는 별이 봅니다. 봄이 까닭이요, 흙으로 어머님, 이름을 거외다. 불러 걱정도 딴은 다 이름과, 별이 책상을 거외다. 남은 새워 이름과, 내 멀듯이, 거외다. 까닭이요, 이웃 하나의 봄이 까닭입니다. 벌써 별 내일 별 동경과 보고, 어머님, 아침이 까닭입니다. 이름을 위에 경, 풀이 노루, 라이너 별들을 않은 없이 거외다.
+
+                    강아지, 풀이 못 밤을 있습니다. 아무 하나에 쓸쓸함과 별 슬퍼하는 하늘에는 봅니다. 나의 내 쓸쓸함과 북간도에 어머니, 까닭입니다. 내린 이웃 나는 시인의 사람들의 있습니다. 까닭이요, 한 이름자를 그리고 듯합니다. 쉬이 무성할 아침이 옥 것은 사랑과 별 언덕 별들을 있습니다. 별 노새, 사랑과 파란 마리아 흙으로 내 라이너 가슴속에 까닭입니다. 오면 아무 벌레는 별 거외다. 까닭이요, 릴케 나는 봅니다. 이름자를 덮어 프랑시스 다하지 거외다. 하나에 잠, 언덕 슬퍼하는 이름과, 있습니다.
+                    </textarea>
+                   
+                    <p> 
+                    <Checkbox
+                      defaultChecked
+                      value="secondary"
+                      color="primary"
+                      inputProps={{ 'aria-label': 'secondary checkbox' }}
+                    />  
+                    개인정보 위탁에 동의합니다. (필수)</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default franchiseInquiry;
+export default FranchiseInquiry;
