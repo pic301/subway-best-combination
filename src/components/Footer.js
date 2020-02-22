16a61f2547dc1637d9de26bfad104d17a9c2eca5
pic301 <ppic301@gmail.com>
2020-02-23 01:36:40 +0900

    Footer 완성
    페이스북 인스타그램 링크추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,15 @@ const Footer = () => {
                 </div>
                
                 <div className="footer-infomation">
+                <div>
                 <p>서울시 서초구 강남대로 535 프린스빌딩 15층 </p> <p>대표 : 콜린클락 </p> <p>전화 : 02-797-5036</p> <p>사업자등록번호 : 101-84-04143</p>
                 <br/>
                 <p>SUBWAY® is a Registered Trademark of Subway IP LLC. © 2019 Subway IP LLC. All Rights Reserved.</p>
+                </div>
+               <div>
+               <a className="sns_instagram" href="https://www.instagram.com/subwaykorea"></a>
+                <a className="sns_facebook" href="https://www.facebook.com/Subwaykr"></a>
+               </div>
                 </div>       
             </div>
 
